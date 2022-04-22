169a481c252f439c9d4ac71c4415e171ebff6d8e
Quentin Gruber <quentingruber@gmail.com>
2022-04-22 16:18:27 +0200

    try to get the right path from the github workspace

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const { context } = require("@actions/github");
 async function run() {
   const { title } = context.payload.pull_request;
   const pull_request_version = title.split("/")[1];
-  const {version} = require("./package.json")
+  const {version} = require(`${process.env.GITHUB_WORKSPACE}/package.json`)
   if(pull_request_version !== version){
     core.setFailed("The version of the pull request is not the same as the version of the package.json");
     core.setFailed("The version of the pull request is: " + pull_request_version);
     core.setFailed("The version of the package.json is: " + version);
+    core.setFailed(`package.json fetched from ${process.env.GITHUB_WORKSPACE}/package.json`);
   }
 }
 
