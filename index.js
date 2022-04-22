dfbab3664cc4d3898e3897d37d3a17dab0af6cb2
Quentin Gruber <quentingruber@gmail.com>
2022-04-22 15:59:24 +0200

    rm unused stuff

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const core = require("@actions/core");
-const { getOctokit, context } = require("@actions/github");
-const fs = require("fs");
+const { context } = require("@actions/github");
 
 async function run() {
-  const repo_token = core.getInput("repo-token");
-  const github = getOctokit(repo_token);
-  // Get informations from the context of the action
   const { title } = context.payload.pull_request;
   const pull_request_version = title.split(" ")[1];
   const {version} = require("/github/workspace/package.json")
