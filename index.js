4b9968f80b813e3996d275d200bb15ffa58cd0b5
Quentin Gruber <quentingruber@gmail.com>
2022-04-22 16:26:30 +0200

    hardcode path

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const { context } = require("@actions/github");
 async function run() {
   const { title } = context.payload.pull_request;
   const pull_request_version = title.split("/")[1];
-  console.log("package.json path : " + process.env.GITHUB_WORKSPACE + "/package.json");
-  console.log(require(`${process.env.GITHUB_WORKSPACE}/package.json`))
-  const version = require(`${process.env.GITHUB_WORKSPACE}/package.json`).version
+  console.log(require(`/home/runner/work/h1z1-server/package.json`))
+  const version = require(`/home/runner/work/h1z1-server/package.json`).version
   console.log("version : " + version);
   if(pull_request_version !== version){
     core.setFailed("The version of the pull request is not the same as the version of the package.json");
