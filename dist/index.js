a3dc990bbe6054f344059c6563d5da7a42f960a7
Quentin Gruber <quentingruber@gmail.com>
2022-04-22 16:22:32 +0200

    log package.json path before trying to use it

--- dist/index.js
+++ dist/index.js
@@ -8710,12 +8710,12 @@ const { context } = __nccwpck_require__(5438);
 async function run() {
   const { title } = context.payload.pull_request;
   const pull_request_version = title.split("/")[1];
+  console.log("package.json path : " + process.env.GITHUB_WORKSPACE + "/package.json");
   const {version} = __ncc_wildcard$0(process.env.GITHUB_WORKSPACE)
   if(pull_request_version !== version){
     core.setFailed("The version of the pull request is not the same as the version of the package.json");
     core.setFailed("The version of the pull request is: " + pull_request_version);
     core.setFailed("The version of the package.json is: " + version);
-    core.setFailed(`package.json fetched from ${process.env.GITHUB_WORKSPACE}/package.json`);
   }
 }
 
