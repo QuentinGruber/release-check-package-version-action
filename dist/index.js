4386521e7f5ae5d7b8a431a5f7e763d0d4e58447
Quentin Gruber <quentingruber@gmail.com>
2022-05-03 19:22:05 +0200

    check that the context contain a pull_request

--- dist/index.js
+++ dist/index.js
@@ -8473,19 +8473,27 @@ const { context } = __nccwpck_require__(5438);
 const path = __nccwpck_require__(1017);
 
 async function run() {
-  const { title } = context.payload.pull_request;
-  const pull_request_version = title.split("/")[1];
-  let dir = process.env.GITHUB_WORKSPACE; 
-  const packagePath = path.join(dir, "package.json");
-  console.log("Load package version from " + packagePath)
-  const version = require(packagePath).version
-  if(pull_request_version !== version){
-    core.setFailed("The version of the pull request is not the same as the version of the package.json");
-    core.setFailed("The version of the pull request is: " + pull_request_version);
-    core.setFailed("The version of the package.json is: " + version);
+  if (context.payload.pull_request) {
+    const { title } = context.payload.pull_request;
+    const pull_request_version = title.split("/")[1];
+    let dir = process.env.GITHUB_WORKSPACE;
+    const packagePath = path.join(dir, "package.json");
+    console.log("Load package version from " + packagePath);
+    const version = require(packagePath).version;
+    if (pull_request_version !== version) {
+      core.setFailed(
+        "The version of the pull request is not the same as the version of the package.json"
+      );
+      core.setFailed(
+        "The version of the pull request is: " + pull_request_version
+      );
+      core.setFailed("The version of the package.json is: " + version);
+    }
+  }
+  else{
+    console.log("No pull request");
   }
 }
-
 
 run();
 
--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const { context } = require("@actions/github");
 const path = require("path");
 
 async function run() {
-  const { title } = context.payload.pull_request;
-  const pull_request_version = title.split("/")[1];
-  let dir = process.env.GITHUB_WORKSPACE; 
-  const packagePath = path.join(dir, "package.json");
-  console.log("Load package version from " + packagePath)
-  const version = require(packagePath).version
-  if(pull_request_version !== version){
-    core.setFailed("The version of the pull request is not the same as the version of the package.json");
-    core.setFailed("The version of the pull request is: " + pull_request_version);
-    core.setFailed("The version of the package.json is: " + version);
+  if (context.payload.pull_request) {
+    const { title } = context.payload.pull_request;
+    const pull_request_version = title.split("/")[1];
+    let dir = process.env.GITHUB_WORKSPACE;
+    const packagePath = path.join(dir, "package.json");
+    console.log("Load package version from " + packagePath);
+    const version = require(packagePath).version;
+    if (pull_request_version !== version) {
+      core.setFailed(
+        "The version of the pull request is not the same as the version of the package.json"
+      );
+      core.setFailed(
+        "The version of the pull request is: " + pull_request_version
+      );
+      core.setFailed("The version of the package.json is: " + version);
+    }
+  }
+  else{
+    console.log("No pull request");
   }
 }
-
 
 run();
